fb202d96dcbfbe5b5c023ca50818b2c164318088
MacUser <macuser@MacUserui-MacBookPro.local>
2024-11-22 18:23:15 +0900

    241122 pado first

---
+++ Desktop/pado/src/app/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const layout = () => {
+  return (
+    <html>
+      <body>
+        <div>Hello Pado</div>
+      </body>
+    </html>
+  );
+};
+
+export default layout;
