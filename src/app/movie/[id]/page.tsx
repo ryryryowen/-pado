8d7adde78819e976e20062d455c624dbb3093a19
GiToon10100011 <Boon10034@gmail.com>
2024-12-03 00:02:18 +0900

    241202 Detail Modal in progress

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import style from "@/styles/detail-modal.module.css";
 import { IMovieData, IResults, ITrailerResults } from "@/types";
+import adultBadge from "@/images/adultTrueBadge.svg";
+import adultFalseBadge from "@/images/adultFalseBadge.svg";
+import { FaPlay } from "react-icons/fa";
+import { IoMdClose } from "react-icons/io";
 import { FaStar } from "react-icons/fa6";
+import { CiStar } from "react-icons/ci";
+import { CiHeart } from "react-icons/ci";
+import Image from "next/image";
 
 const DetailModal = async ({ params }: { params: Promise<{ id: string }> }) => {
   const urls = [
@@ -26,8 +33,6 @@ const DetailModal = async ({ params }: { params: Promise<{ id: string }> }) => {
     ITrailerResults
   ];
 
-  console.log(data, similar, trailerData);
-
   return (
     <div className={style.wrapper}>
       <div className={style.inner}>
@@ -39,42 +44,58 @@ const DetailModal = async ({ params }: { params: Promise<{ id: string }> }) => {
               : `linear-gradient(to right, #162447 30%, rgba(0, 0, 0, 0.3)), url(https://media.themoviedb.org/t/p/original/${data.poster_path}) 200px center/cover no-repeat `,
           }}
         >
-          <div className={style.headerDesc}>
-            <h1 className={style.descTitle}>
-              <span>
-                <FaStar />
-                {parseFloat(data.vote_average).toFixed(2)}
-              </span>
-              {data?.title || data?.original_title}
-              <div className={style.genre}>
-                {data?.genres?.map((item, i, arr) => (
-                  <span className={style.genreItem} key={i}>
-                    {i === arr.length - 1 ? item.name : `${item.name} | `}
-                  </span>
-                ))}
+          <IoMdClose className={style.closeIcon} size={20} />
+          <div className={style.bannerContent}>
+            <div className={style.headerDesc}>
+              <h1 className={style.descTitle}>
+                <span>
+                  <FaStar />
+                  {parseFloat(data.vote_average).toFixed(2)}
+                </span>
+                {data?.title || data?.original_title}
+                <div className={style.genre}>
+                  {data?.genres?.map((item, i, arr) => (
+                    <span className={style.genreItem} key={i}>
+                      {i === arr.length - 1 ? item.name : `${item.name} | `}
+                    </span>
+                  ))}
+                  <Image
+                    src={data.adult ? adultBadge : adultFalseBadge}
+                    alt="연령등급"
+                    className={style.adultBadge}
+                  />
+                </div>
+              </h1>
+              <div className={style.descContent}>
+                <div className={style.playButtonContainer}>
+                  <button className={style.playButton}>
+                    <FaPlay size={16} />
+                  </button>
+                  <span>트레일러 재생하기</span>
+                </div>
+                <div className={style.quickMenu}>
+                  <div className={style.quickMenuItem}>
+                    <CiStar size={28} strokeWidth={0.8}/>
+                    <span>리뷰 남기기</span>
+                  </div>
+                  <div className={style.quickMenuItem}>
+                    <CiHeart size={28} strokeWidth={0.8}/>
+                    <span>보고 싶어요</span>
+                  </div>
+                </div>
+                {/* <span className={style.descDate}>{data?.release_date}</span>
+                <span className={style.descVoteAvg}>{data?.vote_average}</span>
+                <span className={style.descVote}>{data?.vote_count}</span> */}
               </div>
-            </h1>
-            <div className={style.descContent}>
-              <span className={style.descDate}>{data?.release_date}</span>
-              <span className={style.descAdult}>
-                {data?.adult ? "전체관람가" : "18세 이상"}
-              </span>
-              <span className={style.descVoteAvg}>{data?.vote_average}</span>
-              <span className={style.descVote}>{data?.vote_count}</span>
+              <p className={style.descOverview}>{data?.overview}</p>
+              <button className={style.descCheckout}>결제하기</button>
             </div>
-            <div className={style.descBtnGroup}>
-              <button className={style.descBtn}>예고편 재생</button>
-              <button className={style.descBtn}>채팅</button>
-              <button className={style.descBtn}>공유하기</button>
-            </div>
-            <p className={style.descOverview}>{data?.overview}</p>
-            <button className={style.descCheckout}>결제하기</button>
+            <img
+              className={style.headerImg}
+              src={`https://media.themoviedb.org/t/p/original/${data?.poster_path}`}
+              alt="thumbnail"
+            />
           </div>
-          <img
-            className={style.headerImg}
-            src={`https://media.themoviedb.org/t/p/original/${data?.poster_path}`}
-            alt="thumbnail"
-          />
         </div>
         <div className={style.detailContent}>
           <h2 className={style.contentTitle}>영화정보</h2>
